df76d7b6e8c78e7ffcfe9e4be0f1c355ae3c3fd6
egorsv1985 <egorvs2009@gmail.com>
2022-09-04 15:39:38 +0300

    task 1033

--- task1033/server.js
+++ task1033/server.js
@@ -5,5 +5,8 @@ const server = express();
 server.get('/hello', (req, res) => {
   res.send('Hello, Coderslang!');
 })
+server.get('/about', (req, res) => {
+  res.send('Lorem ipsum dolor sit amet');
+})
 
 export { server };
