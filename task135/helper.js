010945a7ed90ef4b6c23dd797a8ef3b85c2334ec
egorsv1985 <egorvs2009@gmail.com>
2022-09-04 12:42:43 +0300

    task 135

--- task135/helper.js
+++ task135/helper.js
@@ -1,3 +1,10 @@
 export const maxElement = (arr) => {
-  return 0;
-}
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+      
+    }
+   
+  }  return max;
+};
