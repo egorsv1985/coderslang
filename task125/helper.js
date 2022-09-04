e37bf268c452e2ca6a939e10540cc99b864ccc78
egorsv1985 <egorvs2009@gmail.com>
2022-09-04 11:36:54 +0300

    task 126

--- task125/helper.js
+++ task125/helper.js
@@ -1,3 +1,11 @@
 export const getBMICategory = (bmi) => {
-  return 'Underweight';
-}
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi > 18.5 && bmi < 25) {
+    return "Normal weight";
+  } else if ((bmi) >= 25 && bmi < 30) {
+    return "Overweight";
+  } else if (bmi >=30) {
+    return "Obesity";
+  }
+};
